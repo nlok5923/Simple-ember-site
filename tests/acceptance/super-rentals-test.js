9799b1469b74f9f8c3857bcfba1086ef8cd37092
Creator5923 <nlokhande5923@gmail.com>
2020-12-06 15:35:33 +0530

    init :tada:

---
+++ tests/acceptance/super-rentals-test.js
@@ -0,0 +1,33 @@
+import { module, test } from 'qunit';
+import { click, visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | super rentals', function(hooks) {
+  setupApplicationTest(hooks);
+   //first visiting root(/) an checking all the html templating 
+  test('visiting /', async function(assert) {
+    await visit('/');
+
+    assert.equal(currentURL(), '/');
+    assert.dom('h2').hasText('Welcome to Super Rentals!');
+
+    assert.dom('.jumbo a.button').hasText('About Us');
+    await click('.jumbo a.button');
+
+    assert.equal(currentURL(), '/about');
+   
+  });
+
+  //visiting /get-in-touch and testing all /get-in-touch functinality 
+  test('visiting /getting-in-touch', async function(assert) {
+    await visit('/getting-in-touch');
+
+    assert.equal(currentURL(), '/getting-in-touch');
+    assert.dom('h2').hasText('Contact Us');
+
+    assert.dom('.jumbo a.button').hasText('About');
+    await click('.jumbo a.button');
+
+    assert.equal(currentURL(), '/about');
+  });
+});
